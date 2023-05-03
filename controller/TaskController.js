c441d29589b38b9afe4807f47fea9673d8f1380c
Guilherme Steglich de Oliveira <guicrga@gmail.com>
2023-05-03 16:23:56 -0300

    :sparkles: feat: Create check func

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -90,11 +90,30 @@ const deleteTask = async (req, res) => {
     }
 }
 
+const taskCheck = async (req, res) => {
+    try {
+        const task = await  Task.findOne({_id: req.params.id})
+
+        task.check ? task.check = false : task.check = true
+        
+        // if (task.check){
+        //     task.check = false
+        // } else {
+        //     task.check = true
+        // }
+
+        await Task.updateOne({_id: req.params.id}, task)
+        res.redirect("/")
+    } catch (err) {
+        res.status(500).send({error: err.message})
+    }
+}
 
 module.exports = {
     getAllTasks,
     createTask,
     getById,
     updateTask,
-    deleteTask
+    deleteTask,
+    taskCheck
 }
